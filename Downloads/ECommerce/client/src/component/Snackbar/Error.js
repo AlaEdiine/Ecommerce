8868462f870b100c5f11b33a4b5986cf360942d4
Ala.Eddine <143133176+AlaEdiine@users.noreply.github.com>
2023-10-23 20:40:15 +0100

    Delete Downloads/ECommerce directory

--- Downloads/ECommerce/client/src/component/Snackbar/Error.js
+++
@@ -1,7 +0,0 @@
-// import { SnackbarProvider, enqueueSnackbar } from 'notistack';
-import { enqueueSnackbar } from "notistack";
-
-const Error = (message , variant) => {
-  enqueueSnackbar( message, { variant });
-};
-export default Error;
--- Downloads/ECommerce/server/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { payment } = require("../authcontrollers/api");
-
-// payment
-router.post('/sessionCheckout', payment)
-
-module.exports = router
